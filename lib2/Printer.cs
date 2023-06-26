59c891db943cda873f4dadefc956933ea05e28ff
Arturo Palau Haas <arturo.palau@sonarsource.com>
2023-06-26 19:49:40 +0000

    Added Printer.cs

---
+++ lib2/Printer.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Library2
+{
+    public class Printer
+    {
+        public void PrintMessage(string message)
+        {
+            Console.WriteLine("Printing: " + message);
+        }
+    }
+}
